2e37c4b69d621a999a432bff4feba804a66028aa
c10023596 <denizdonmez@std.iyte.edu.tr>
2020-10-19 09:48:25 +0000

    I tried to modify 3, but failed drastically.

--- lab2/3.py
+++ lab2/3.py
@@ -1,6 +1,12 @@
-a = 2
-b = 6
-c = 20
-x_p = (-b + ((b**2) - (4*a*c))**0.5) / (2*a)
-x_n = (-b - ((b**2) - (4*a*c))**0.5) / (2*a)
-print(x_p , x_n)
+a = input("Please input the value of a:")
+b = input("Please input the value of b:")
+c = input("Please input the value of c:")
+
+p_x_top = -b + ((b**2) - (4*a*c))
+n_x_top = -b - ((b**2) - (4*a*c))
+x_bot = 2*a
+
+p_x = p_x_top / x_bot
+n_x = n_x_top / x_bot
+
+print("The roots are:" (p_x , n_x))
